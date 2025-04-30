a4e09d04d0f5d6899932fff25d19a71490b2c606
TimEpsilon <tim.dewachter@live.fr>
2025-04-30 19:50:26 +0200

    Convert Recipes to large json
    Signed-off-by: TimEpsilon <tim.dewachter@live.fr>

--- src/main/java/com/github/timepsilon/server/commands/GenerateEquivalency.java
+++ src/main/java/com/github/timepsilon/server/commands/GenerateEquivalency.java
@@ -1,5 +1,6 @@
 package com.github.timepsilon.server.commands;
 
+import com.google.gson.Gson;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
@@ -10,6 +11,8 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.*;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -44,40 +47,60 @@ public class GenerateEquivalency {
         Files.write(Path.of("C:/Users/timde/Documents/items.txt"), items);
     }
 
-    private static void iterateRecipes(CommandSourceStack source) {
+    private static void iterateRecipes(CommandSourceStack source) throws IOException {
+        // Getting the recipes
         RecipeManager recipeManager = source.getServer().getRecipeManager();
-
         Collection<RecipeHolder<?>> recipes = recipeManager.getRecipes();
 
+        // Soon to be JSON mapping the recipe ids to their input, output and type
+        HashMap<String, Object> recipeMap = new HashMap<>();
+
         for (RecipeHolder<?> holder : recipes) {
+            // Getting the real recipe object out of the holder
             Recipe<?> recipe = holder.value();
-            ResourceLocation id = holder.id();
-            RecipeType<?> type = recipe.getType();
 
-            System.out.println("Recipe ID: " + id + " Type: " + type);
+            HashMap<String,Object> singleRecipeDict = new HashMap<>();
 
-            // Try to extract inputs
-            List<ItemStack> inputs = getInputs(recipe);
-            System.out.println("  Inputs:");
-            System.out.println(inputs);
+            // Extract a map of items <-> amount
+            HashMap<String, Integer> inputMap = getInputs(recipe);
 
-            // Try to extract output
+            // Extract a map of items <-> amount
+            HashMap<String, Integer> outputMap = new HashMap<>();
             ItemStack output = recipe.getResultItem(source.getServer().registryAccess());
             if (!output.isEmpty()) {
-                System.out.println(output);
-            } else {
-                System.out.println("  Output: <none>");
+                outputMap.put(output.getItem().toString(), output.getCount());
             }
+
+            // Constructing the dict for a single recipe
+            singleRecipeDict.put("type", recipe.getType().toString());
+            singleRecipeDict.put("input", inputMap);
+            singleRecipeDict.put("output", outputMap);
+
+            // Adding the dict to the main dict under the key id
+            recipeMap.put(holder.id().toString(), singleRecipeDict);
         }
+
+        // Serialization
+        Gson gson = new Gson();
+        String json = gson.toJson(recipeMap);
+
+        // Saving
+        BufferedWriter writer = new BufferedWriter(new FileWriter("C:/Users/timde/Documents/recipes.json"));
+        writer.write(json);
+        writer.close();
+
     }
 
-    private static List<ItemStack> getInputs(Recipe<?> recipe) {
-        List<ItemStack> inputs = new ArrayList<>();
+    private static HashMap<String, Integer> getInputs(Recipe<?> recipe) {
         List<Ingredient> ingredients = recipe.getIngredients();
+        HashMap<String, Integer> inputMap = new HashMap<>();
         for (Ingredient ingredient : ingredients) {
-            inputs.addAll(Arrays.asList(ingredient.getItems()));
+            System.out.println(ingredient.getItems());
+            if (ingredient.getItems().length > 0) {
+                inputMap.put(ingredient.getItems()[0].getItem().toString(), ingredient.getItems().length);
+            }
         }
-        return inputs;
+        return inputMap;
     }
 
 }
